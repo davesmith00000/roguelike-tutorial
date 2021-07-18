5153bb0dd6b41a816944dd393b48f638446523b1
Dave Smith <davesmith.00000@gmail.com>
2021-07-18 21:09:29 +0100

    Added new colors

--- part8/src/main/scala/roguelike/ColorScheme.scala
+++ part8/src/main/scala/roguelike/ColorScheme.scala
@@ -4,7 +4,12 @@ import indigo.shared.datatypes.RGB
 
 object ColorScheme:
 
-  val welcomeText: RGB = RGB.fromHexString("20A0FF")
+  val invalid: RGB    = RGB.Yellow
+  val impossible: RGB = RGB.fromHexString("808080")
+  val error: RGB      = RGB.fromHexString("FF4040")
+
+  val welcomeText: RGB     = RGB.fromHexString("20A0FF")
+  val healthRecovered: RGB = RGB.Green
 
   val barText: RGB   = RGB.White
   val barFilled: RGB = RGB.fromHexString("006000")
